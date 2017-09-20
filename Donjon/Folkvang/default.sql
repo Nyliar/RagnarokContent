5e70f82aa82226635f522a3343a93c594e33b540
Pierre Pouzol <Pierre@Visioscopie.local>
2017-09-20 11:35:12 +0200

    Added Folkvang Dungeon

---
+++ Donjon/Folkvang/default.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `folkvang_clone` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class` int(11) NOT NULL,
+  `count` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
